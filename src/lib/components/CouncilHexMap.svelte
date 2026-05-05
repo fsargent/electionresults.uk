3d15a76cf24f77e6c0e1146f0aedb496410e0228
Felix Sargent <felix.sargent@snyk.io>
2026-05-05 14:36:40 +0100

    chore: silence the second a11y warning on the hex SVG

--- src/lib/components/CouncilHexMap.svelte
+++ src/lib/components/CouncilHexMap.svelte
@@ -113,7 +113,8 @@
 </script>
 
 <figure class="hex-map">
-  <!-- svelte-ignore a11y_no_noninteractive_element_interactions a11y_click_events_have_key_events -->
+  <!-- svelte-ignore a11y_click_events_have_key_events -->
+  <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
   <svg
     viewBox={`0 0 ${layout.width} ${layout.height}`}
     role="img"
